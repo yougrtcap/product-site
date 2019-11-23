262e3ac3f00aad6fb56c429241456b6fb8510bb1
kiichi <yougrtcap@gmail.com>
2019-11-23 16:31:18 +0900

    PhotoFormを使ってエラー回避

--- app/views.py
+++ app/views.py
@@ -76,7 +76,9 @@ def photos_category(request, category):
 
 
 def edit_photo(request, pk):
+    form = PhotoForm()
     photo = get_object_or_404(Photo, pk=pk)
+
     if request.method == "POST":
         form = PhotoForm(request.POST, instance=photo)
         if form.is_valid():
