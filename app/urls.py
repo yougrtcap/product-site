62454e7f355ad68a836d6139e60534fea284880b
kiichi <yougrtcap@gmail.com>
2019-11-23 15:43:23 +0900

    いいね機能をつける前

--- app/urls.py
+++ app/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('photos/new/', views.photos_new, name='photos_new'),
     path('photos/<int:pk>/', views.photos_detail, name='photos_detail'),
     path('photos/<int:pk>/delete/', views.photos_delete, name='photos_delete'),
+    path('photos/<int:pk>/edit/', views.edit_photo, name='edit_photo'),
     path('photos/<str:category>/', views.photos_category, name='photos_category'),
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='app/login.html'), name='login'),
